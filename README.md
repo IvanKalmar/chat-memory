c3a89b2c5d0251de79267bba7c96b5a8f54b287d
kalmar <ivan.kalmar6@gmail.com>
2025-03-28 12:57:05 +0300

    Test update;

--- README.md
+++ README.md
@@ -1,33 +1,29 @@
-# SillyTavern Extension Example
+# SillyTavern Extension `ChatMemory`
 
-*Provide a brief description of how your extension works, what problem it aims to solve.*
+Control dialog context via code
 
 ## Features
 
-*Describe some of the main selling points of your extension.*
+Execution of pre and post message scripts<br/>
 
 ## Installation and Usage
 
 ### Installation
 
-*In most cases, this should just be using ST's inbuilt extension installer.* 
+Extensions -> Install extensions -> Past url of this repository
 
 ### Usage
 
-*Explain how to use this extension.*
+-
 
 ## Prerequisites
 
-*Specify the version of ST necessary here.*
+-
 
 ## Support and Contributions
 
-*Where should someone ask for support?*
-
-*Consider including your own contact info for help/questions.*
-
-*How can people help add to this extension?*
+-
 
 ## License
 
-*Be cool, use an open source license.*
+MIT License
--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-// The main script for the extension
-// The following are examples of some basic extension functionality
-
-//You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
 import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
 
-//You'll likely need to import some other functions from the main script
 import { saveSettingsDebounced } from "../../../../script.js";
 
-// Keep track of where your extension is located, name should match repo name
-const extensionName = "st-extension-example";
+
+const extensionName = "chat-memory";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 const extensionSettings = extension_settings[extensionName];
 const defaultSettings = {};
 
 
- 
-// Loads the extension settings if they exist, otherwise initializes them to the defaults.
+jQuery(async () => {
+  const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
+
+  $("#extensions_settings").append(settingsHtml);
+
+  $("#my_button").on("click", onButtonClick);
+  $("#example_setting").on("input", onExampleInput);
+
+  await loadSettings();
+});
+
+
 async function loadSettings() {
-  //Create the settings if they don't exist
   extension_settings[extensionName] = extension_settings[extensionName] || {};
   if (Object.keys(extension_settings[extensionName]).length === 0) {
     Object.assign(extension_settings[extensionName], defaultSettings);
   }
 
-  // Updating settings in the UI
   $("#example_setting").prop("checked", extension_settings[extensionName].example_setting).trigger("input");
 }
 
-// This function is called when the extension settings are changed in the UI
+/*
+
+
 function onExampleInput(event) {
   const value = Boolean($(event.target).prop("checked"));
   extension_settings[extensionName].example_setting = value;
   saveSettingsDebounced();
 }
 
-// This function is called when the button is clicked
 function onButtonClick() {
   // You can do whatever you want here
   // Let's make a popup appear with the checked setting
@@ -61,3 +65,4 @@ jQuery(async () => {
   // Load settings when starting things up (if you have any)
   loadSettings();
 });
+*/
