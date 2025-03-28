9166838a9679a07f4244a279b56ac54fe4834b27
kalmar <ivan.kalmar6@gmail.com>
2025-03-28 14:01:07 +0300

    Test update;

--- index.js
+++ index.js
@@ -15,32 +15,56 @@ jQuery(async () => {
   const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
   $("#extensions_settings").append(settingsHtml);
 
+  const enabledChats = $("#chat_memory_enabled_chats");
   const availableChats = $("#chat_memory_available_chats");
+  const addChatButton = $("#chat_memory_add_chat_button");
+  const removeChatButton = $("#chat_memory_remove_chat_button");
   const saveButton = $("#chat_memory_save_settings_button");
 
-  saveButton.on("click", () => {
-    extensionSettings.selectedChats = availableChats.val();
-    saveSettingsDebounced();
-    toastr.info(``, "Saved!");
-  });
+  const updateChats = () => {
+    for(const chat of extension_settings[extensionName].selectedChats) {
+      enabledChats.append(`<p>${chat}</p>`)
+    }
+  }
 
   extension_settings[extensionName] = extension_settings[extensionName] || {};
   if (Object.keys(extension_settings[extensionName]).length === 0) {
     Object.assign(extension_settings[extensionName], defaultSettings);
   }
 
+  updateChats();
+
   const context = SillyTavern.getContext();
 
   eventSource.on(event_types.CHARACTER_PAGE_LOADED, () => {
     availableChats.empty();
 
-    for(const c in context.characters) {
-      console.log(c);
+    for(const c of context.characters) {
       availableChats.append(new Option(c.name, c.name));
     }
-
-    availableChats.val(extension_settings[extensionName].selectedChats);
   })
+
+
+  addChatButton.on("click", () => {
+    const selectedChats = new Set(extensionSettings.selectedChats);
+    selectedChats.add(availableChats.val());
+    extensionSettings.selectedChats = Array.from(selectedChats);
+
+    updateChats();
+  });
+
+  removeChatButton.on("click", () => {
+    const selectedChats = new Set(extensionSettings.selectedChats);
+    selectedChats.delete(availableChats.val());
+    extensionSettings.selectedChats = Array.from(selectedChats);
+
+    updateChats();
+  });
+
+  saveButton.on("click", () => {
+    saveSettingsDebounced();
+    toastr.info(``, "Saved!");
+  });
 
   eventSource.on(event_types.MESSAGE_RECEIVED, (messageIndex) => {
     const message = context.chat[messageIndex].mes;
