30802f0096fe84d6fe9dcf730b11bb9570f4064f
kalmar <ivan.kalmar6@gmail.com>
2025-03-28 16:16:06 +0300

    Test update;

--- index.js
+++ index.js
@@ -35,8 +35,7 @@ jQuery(async () => {
   const context = SillyTavern.getContext();
 
   eventSource.on(event_types.CHAT_CHANGED, (messageIndex) => {
-    console.log("!!!", context.chatId);
-    console.log("!!!", context);
+    console.log("!!!", context.getCurrentChatId());
   });
 
   eventSource.on(event_types.MESSAGE_RECEIVED, (messageIndex) => {
